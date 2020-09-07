35db644d917fac64b6c2705e6eee362c14f67aae
Calvin Sung <calvinsungvin@gmail.com>
2020-09-07 15:51:36 +0800

    add flash messages

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const db = require('./models')
 const app = express()
+const bodyParser = require('body-parser') 
 const port = 3000
+const flash = require('connect-flash')
+const session = require('express-session')
 
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({ 
+    secret: 'secret', 
+    resave: false, 
+    saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    next()
+  })
+
+
 
 
 app.listen(port, () => {
+    db.sequelize.sync()
     console.log(`Example app listening on port ${port}`)
 })
+
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app)
