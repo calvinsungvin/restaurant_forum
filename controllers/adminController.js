361fe1f91b8b2763af24055583010e8e55475ffe
Calvin Sung <calvinsungvin@gmail.com>
2020-09-08 12:21:35 +0800

    Add admin restaurant create form & post features

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -10,6 +10,26 @@ let adminController = {
             })
 
         })
+    },
+    createRestaurant: (req, res) => {
+        return res.render('admin/create')
+    },
+    postRestaurant: (req, res) => {
+        if (!req.body.name) {
+            req.flash('error_messages', "name didn't exist")
+            return res.redirect('back')
+        }
+        return Restaurant.create({
+            name: req.body.name,
+            rel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description
+        })
+        .then((restaurant) => {
+            req.flash('sucess_messages', 'restaurant was successfully created')
+            res.redirect('/admin/restaurants')
+        })
     }
 }
 
