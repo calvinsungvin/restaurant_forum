b1873376d38a02f24332e9f14a46217b7eac55b4
Calvin Sung <calvinsungvin@gmail.com>
2020-09-14 12:30:57 +0800

    Add categories selector on admin create and edit page/ Add CategoryId to admin postRestaurant and putRestaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -21,8 +21,15 @@ let adminController = {
         })
     },
     createRestaurant: (req, res) => {
-        return res.render('admin/create')
-    },
+      Category.findAll({ 
+        raw: true,
+        nest: true
+      }).then(categories => {
+        return res.render('admin/create', {
+          categories: categories
+        })
+      })
+     },
     postRestaurant: (req, res) => {
       if(!req.body.name){
         req.flash('error_messages', "name didn't exist")
@@ -40,6 +47,7 @@ let adminController = {
             opening_hours: req.body.opening_hours,
             description: req.body.description,
             image: file ? img.data.link : null,
+            CategoryId: req.body.categoryId
           }).then((restaurant) => {
             req.flash('success_messages', 'restaurant was successfully created')
             return res.redirect('/admin/restaurants')
@@ -53,7 +61,8 @@ let adminController = {
           address: req.body.address,
           opening_hours: req.body.opening_hours,
           description: req.body.description,
-          image: null
+          image: null,
+          CategoryId: req.body.categoryId
         }).then((restaurant) => {
           req.flash('success_messages', 'restaurant was successfully created')
           return res.redirect('/admin/restaurants')
@@ -68,10 +77,21 @@ let adminController = {
         })
       },
     editRestaurant: (req, res) => {
-        return Restaurant.findByPk(req.params.id, {raw: true}).then(restaurant => {
-            return res.render('admin/create', { restaurant: restaurant })
+      Category.findAll({
+        raw: true,
+        nest: true
+      }).then(categories => {
+        return Restaurant.findByPk(req.params.id).then(restaurant => {
+          return res.render('admin/create', {
+            categories: categories, 
+            restaurant: restaurant.toJSON()
+          })
         })
+      })
     },
+
+
+
     putRestaurant: (req, res) => {
       if(!req.body.name){
         req.flash('error_messages', "name didn't exist")
@@ -91,6 +111,7 @@ let adminController = {
                 opening_hours: req.body.opening_hours,
                 description: req.body.description,
                 image: file ? img.data.link : restaurant.image,
+                CategoryId: req.body.categoryId
               })
               .then((restaurant) => {
                 req.flash('success_messages', 'restaurant was successfully to update')
@@ -108,7 +129,8 @@ let adminController = {
               address: req.body.address,
               opening_hours: req.body.opening_hours,
               description: req.body.description,
-              image: restaurant.image
+              image: restaurant.image,
+              CategoryId: req.body.categoryId
             })
             .then((restaurant) => {
               req.flash('success_messages', 'restaurant was successfully to update')
