3c385e5ebfb287e2f13ca5b6aad559a73f8edf08
Calvin Sung <calvinsungvin@gmail.com>
2020-09-15 17:03:52 +0800

    add user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
+const fs = require('fs')
 const bcrypt = require('bcryptjs') 
 const db = require('../models')
 const User = db.User
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = 'e7b642f2d94bb82'
+
 
 const userController = {
   signUpPage: (req, res) => {
@@ -42,6 +46,53 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        return res.render('userProfile', { profileUser: user.toJSON() })
+      })
+  },
+  editUser: (req, res) => {
+    // return User.findByPk(req.params.id)
+    //   .then(user => {
+    //     return res.render('editProfile')
+    //   })
+    res.render('editProfile')
+  },
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', 'name did not exist')
+      return res.reditect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then(user => {
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : null
+            }).then(user => {
+              req.flash('success_messages', 'user info is now updated!')
+              res.redirect(`/users/${user.id}`)
+            })
+          })
+      })
+    } else {
+      return User.findByPk(req.params.id)
+        .then(user => {
+          user.update({
+            name: req.body.name,
+            image: user.image
+          }).then(user => {
+            req.flash('success_messages', 'user info is now updated!')
+            res.redirect(`/users/${user.id}`)
+          })
+        })
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,9 @@ module.exports = (app, passport) => {
     app.get('/restaurants/:id', authenticated, restController.getRestaurant)
     app.post('/comments', authenticated, commentController.postComment)
     app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
-
+    app.get('/users/:id', authenticated, userController.getUser)
+    app.get('/users/:id/edit', authenticated, userController.editUser)
+    app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 
 
