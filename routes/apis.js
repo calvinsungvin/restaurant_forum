96a5582cf2621761e90ccb72a071746abc3fa675
Calvin Sung <calvinsungvin@gmail.com>
2020-09-21 14:18:57 +0800

    refactor adminController.deleteRestaurant

--- routes/apis.js
+++ routes/apis.js
@@ -9,5 +9,7 @@ router.get('/admin/restaurants/:id', adminController.getRestaurant)
 
 router.get('/admin/categories', categoryController.getCategories)
 
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+
 
 module.exports = router
--- services/adminService.js
+++ services/adminService.js
@@ -23,6 +23,15 @@ let adminService = {
             .then(restaurant => {
                 callback({ restaurant: restaurant })
         })
+    },
+    deleteRestaurant: (req, res, callback) => {
+      return Restaurant.findByPk(req.params.id)
+        .then((restaurant) => {
+          restaurant.destroy()
+            .then((restaurant) => {
+              callback({ status:'success', message: '' })
+            })
+        })
     }
 }
 
